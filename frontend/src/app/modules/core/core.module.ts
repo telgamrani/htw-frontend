b553c68994cd7387f67ded8d8a6ff7e1a4a4ac1d
taoufik <taoufik.elgamrani@gmail.com>
2019-01-07 23:49:38 +0100

    feat(footer) : add footer component

--- frontend/src/app/modules/core/core.module.ts
+++ frontend/src/app/modules/core/core.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderModule } from './modules/header/header.module';
 import { SubHeaderModule } from './modules/sub-header/sub-header.module';
+import { FooterModule } from './modules/footer/footer.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     HeaderModule,
-    SubHeaderModule
+    SubHeaderModule,
+    FooterModule
   ],
   exports: [
     HeaderModule,
-    SubHeaderModule
+    SubHeaderModule,
+    FooterModule
   ]
 })
 export class CoreModule { }
